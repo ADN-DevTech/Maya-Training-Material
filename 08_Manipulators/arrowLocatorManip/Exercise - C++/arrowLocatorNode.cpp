ef5d07415b539408e4f973b1f2d9df0a4ac295bc
wengn <naiqi.weng@autodesk.com>
2014-09-24 17:44:14 -0400

    Update sample/exercises in Lesson 8
    Update custom locator node to work with VP 2.0

--- 08_Manipulators/arrowLocatorManip/Exercise - C++/arrowLocatorNode.cpp
+++ 08_Manipulators/arrowLocatorManip/Exercise - C++/arrowLocatorNode.cpp
@@ -26,6 +26,9 @@
 //
 //change the following to a unique value 
 MTypeId     arrowLocator::id( 0x00002 );
+MString	arrowLocator::drawDbClassification("drawdb/geometry/arrowLocator");
+MString	arrowLocator::drawRegistrantId("arrowLocatorNodePlugin");
+
 
 MObject     arrowLocator::windDirection;
 
@@ -179,4 +182,258 @@ MBoundingBox arrowLocator::boundingBox() const
 
 	MBoundingBox boundingArea(upLeftCorner,downRightCorner);
 	return boundingArea;
+}
+
+
+void arrowLocator::getRotationAngle (arrowLocatorData * data){
+	// Retrieve value of the angle attribute from the node
+	MStatus status ;
+	
+	MObject node = thisMObject();
+	MPlug wdPlug(node, windDirection);
+	MAngle angle;
+	wdPlug.getValue(angle);
+
+	data->rotateAngle = angle;
+}
+
+
+
+class arrowLocatorOverride : public MHWRender::MPxDrawOverride
+{
+public:
+	static MHWRender::MPxDrawOverride* Creator(const MObject& obj)
+	{
+		return new arrowLocatorOverride(obj);
+	}
+
+	virtual ~arrowLocatorOverride();
+
+	virtual MHWRender::DrawAPI supportedDrawAPIs() const;
+
+    virtual bool isBounded(
+        const MDagPath& objPath,
+        const MDagPath& cameraPath) const;
+
+	virtual MBoundingBox boundingBox(
+		const MDagPath& objPath,
+		const MDagPath& cameraPath) const;
+
+	virtual MUserData* prepareForDraw(
+		const MDagPath& objPath,
+		const MDagPath& cameraPath,
+		const MHWRender::MFrameContext& frameContext,
+		MUserData* oldData);
+
+	static void draw(
+		const MHWRender::MDrawContext& context,
+		const MUserData* data);
+
+private:
+	arrowLocatorOverride(const MObject& obj);
+};
+
+
+
+arrowLocatorOverride::arrowLocatorOverride(const MObject& obj)
+: MHWRender::MPxDrawOverride(obj, arrowLocatorOverride::draw)
+{
+}
+
+arrowLocatorOverride::~arrowLocatorOverride()
+{
+}
+
+MHWRender::DrawAPI arrowLocatorOverride::supportedDrawAPIs() const
+{
+	return MHWRender::kOpenGL;
+}
+
+bool arrowLocatorOverride::isBounded(
+	const MDagPath& objPath,
+	const MDagPath& cameraPath) const
+{
+	return true;
+}
+
+MBoundingBox arrowLocatorOverride::boundingBox(
+	const MDagPath& objPath,
+	const MDagPath& cameraPath) const
+{
+	MStatus status;
+	MFnDependencyNode node(objPath.node(), &status);
+	arrowLocator* locatorNode =
+		dynamic_cast<arrowLocator*>(node.userNode());
+	return locatorNode->boundingBox();
+}
+
+MUserData* arrowLocatorOverride::prepareForDraw(
+	const MDagPath& objPath,
+	const MDagPath& cameraPath,
+	const MHWRender::MFrameContext& frameContext,
+	MUserData* oldData)
+{
+	// get the node
+	MStatus status;
+	MFnDependencyNode node(objPath.node(), &status);
+	if (!status) return NULL;
+	arrowLocator* locatorNode =
+		dynamic_cast<arrowLocator*>(node.userNode());
+	if (!locatorNode) return NULL;
+
+	// access/create user data for draw callback
+	arrowLocatorData* data =
+		dynamic_cast<arrowLocatorData*>(oldData);
+	if (!data)
+	{
+		data = new arrowLocatorData();
+	}
+
+	// compute data and cache it
+	locatorNode->getRotationAngle(data) ;
+
+	return data;
+}
+
+void arrowLocatorOverride::draw(
+	const MHWRender::MDrawContext& context,
+	const MUserData* data)
+{
+	MAngle rotationAngle;
+
+	float color [3] ={ 0.0f, 1.0f, 0.0f } ;
+
+	// data
+	MStatus status;
+	MHWRender::MStateManager* stateMgr = context.getStateManager();
+	const arrowLocatorData* locatorData =
+		dynamic_cast<const arrowLocatorData*>(data);
+	if (!stateMgr || !locatorData) return;
+
+	if ( locatorData )
+		rotationAngle = locatorData->rotateAngle;
+
+	// matrices
+	const MMatrix transform =
+		context.getMatrix(MHWRender::MFrameContext::kWorldViewMtx, &status);
+	if (status != MStatus::kSuccess) return;
+	const MMatrix projection =
+		context.getMatrix(MHWRender::MFrameContext::kProjectionMtx, &status);
+	if (status != MStatus::kSuccess) return;
+
+	// get renderer
+	MHWRender::MRenderer *theRenderer =MHWRender::MRenderer::theRenderer () ;
+	if ( !theRenderer )
+		return ;
+	if ( theRenderer->drawAPIIsOpenGL () ) {
+		
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+		glLoadMatrixd(transform.matrix[0]);
+		glMatrixMode(GL_PROJECTION);
+		glPushMatrix();
+		glLoadMatrixd(projection.matrix[0]);
+
+		glPushAttrib(GL_CURRENT_BIT);
+		glColor4fv(color);
+
+		//- Draw the outline of the arrow shape
+		glBegin( GL_LINE_STRIP);
+		glRotated(-rotationAngle.asDegrees(), 0.0, 1.0, 0.0);
+		glVertex3f(arrow[0][0],arrow[0][1],arrow[0][2]);
+		glVertex3f(arrow[1][0],arrow[1][1],arrow[1][2]);
+		glVertex3f(arrow[2][0],arrow[2][1],arrow[2][2]);
+		glEnd();
+
+		glBegin( GL_LINE_STRIP );
+		glVertex3f(arrow[2][0],arrow[2][1],arrow[2][2]);
+		glVertex3f(arrow[3][0],arrow[3][1],arrow[3][2]);
+		glVertex3f(arrow[0][0],arrow[0][1],arrow[0][2]);
+		glEnd();
+
+	
+		glPopAttrib();
+
+		glPopMatrix();
+		glMatrixMode(GL_MODELVIEW);
+		glPopMatrix();
+
+	}
+}
+
+MStatus initializePlugin( MObject obj )
+//
+//	Description:
+//		this method is called when the plug-in is loaded into Maya.  It 
+//		registers all of the services that this plug-in provides with 
+//		Maya.
+//
+//	Arguments:
+//		obj - a handle to the plug-in object (use MFnPlugin to access it)
+//
+{ 
+	MStatus   status;
+	MFnPlugin plugin( obj, "", "2009", "Any");
+
+	status = plugin.registerNode( "arrowLocator", arrowLocator::id, arrowLocator::creator,
+		arrowLocator::initialize, MPxNode::kLocatorNode,&arrowLocator::drawDbClassification);
+
+	status = MHWRender::MDrawRegistry::registerDrawOverrideCreator (
+		arrowLocator::drawDbClassification,
+		arrowLocator::drawRegistrantId,
+		arrowLocatorOverride::Creator) ;
+
+	if (!status) {
+		status.perror("registerNode");
+		return status;
+	}
+
+
+	//- Register custom manipulator for your custom locator node.
+	//- In order to make it work, you need to name your custom manipulator 
+	//- after your custom node type name, also in your custom node's initialize()
+	//- function, you need to call MPxManipContainer::addToManipConnectTable()
+
+	status = plugin.registerNode( "arrowLocatorManip", arrowLocatorManip::id, arrowLocatorManip::creator,
+		arrowLocatorManip::initialize,MPxNode::kManipContainer );
+	if (!status) {
+		status.perror("registerNode");
+		return status;
+	}
+
+
+	return status;
+}
+
+MStatus uninitializePlugin( MObject obj)
+//
+//	Description:
+//		this method is called when the plug-in is unloaded from Maya. It 
+//		deregisters all of the services that it was providing.
+//
+//	Arguments:
+//		obj - a handle to the plug-in object (use MFnPlugin to access it)
+//
+{
+	MStatus   status;
+	MFnPlugin plugin( obj );
+
+	status = plugin.deregisterNode( arrowLocatorManip::id );
+	if (!status) {
+		status.perror("deregisterNode");
+		return status;
+	}
+
+	status = plugin.deregisterNode( arrowLocator::id );
+
+	MHWRender::MDrawRegistry::deregisterDrawOverrideCreator (
+		arrowLocator::drawDbClassification,
+		arrowLocator::drawRegistrantId) ;
+
+	if (!status) {
+		status.perror("deregisterNode");
+		return status;
+	}
+
+	return status;
 }
--- 08_Manipulators/arrowLocatorManip/Solution - C++/arrowLocatorNode.h
+++ 08_Manipulators/arrowLocatorManip/Solution - C++/arrowLocatorNode.h
@@ -16,31 +16,65 @@
 #include <maya/M3dView.h>
 #include <maya/MDagPath.h>
 
-//macros to use to rotate the locator
+
+// Viewport 2.0 includes
+#include <maya/MDrawRegistry.h>
+#include <maya/MPxDrawOverride.h>
+#include <maya/MUserData.h>
+#include <maya/MDrawContext.h>
+#include <maya/MGlobal.h>
+#include <maya/MSelectionList.h>
+#include <maya/MViewport2Renderer.h>
+#include <maya/MHWGeometryUtilities.h>
+#include <maya/MStateManager.h>
+#include <maya/MShaderManager.h>
+#include <maya/MAngle.h>
+
+
+
+//- Macros to use to rotate the locator
 #define PI 3.14159265358979
 #define toDegree(rot) rot*(180.0/PI)
+
+class arrowLocatorData : public MUserData
+{
+public:
+	MAngle rotateAngle ;
+
+	arrowLocatorData() : MUserData(false) {} // don't delete after draw
+	virtual ~arrowLocatorData() {}
+};
 
 class arrowLocator : public MPxLocatorNode
 {
 public:
-						arrowLocator();
-	virtual				~arrowLocator(); 
+	arrowLocator() {}
+	virtual ~arrowLocator() {}
 
-	virtual MStatus		compute( const MPlug& plug, MDataBlock& data );
-
-	virtual void		draw(M3dView &view,const MDagPath & path,M3dView::DisplayStyle style, M3dView::DisplayStatus status);
-	virtual bool		isBounded() const;
+	MStatus compute( const MPlug& plug, MDataBlock& data );
+	virtual void draw(M3dView &view,const MDagPath & path,M3dView::DisplayStyle style, M3dView::DisplayStatus status);
 	virtual MBoundingBox boundingBox() const;
+	virtual bool isBounded() const;
 
-	static  void*		creator();
-	static  MStatus		initialize();
 
+	void getRotationAngle (arrowLocatorData * data );
+
+	static MStatus		initialize();
+
+	static void* creator() {
+		return new arrowLocator();
+	}
+
+#endif
+
+	
 public:
-
 	static	MTypeId		id;
 
-	static MObject windDirection; // this is an attribute representing the rotation angle of this locator
+	static	MString		drawDbClassification;
+	static	MString		drawRegistrantId;
+
+	static MObject windDirection; //- This is an attribute representing the rotation angle
 
 };
 
-#endif
